5cc2be776071c181f313099acf5fc96419848e2a
vinimax <vini.ciusang@gmail.com>
2026-01-23 12:42:33 -0300

    feat(domain): add JPA support to delivery entities
    - Map Delivery entity and its relationships
    - Configure Value Objects with @Embeddable
    - Apply @AttributeOverrides for ContactPoint mapping
    - Define primary key strategies and column names

--- Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/ContactPoint.java
+++ Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/ContactPoint.java
@@ -1,14 +1,14 @@
 package com.algaworks.algadelivery.delivery.tracking.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import jakarta.persistence.Embeddable;
+import lombok.*;
 
+@Embeddable
 @EqualsAndHashCode
 @AllArgsConstructor
 @Builder
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ContactPoint {
     private String zipCode;
     private String street;
--- Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Delivery.java
+++ Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Delivery.java
@@ -1,6 +1,7 @@
 package com.algaworks.algadelivery.delivery.tracking.domain.model;
 
 import com.algaworks.algadelivery.delivery.tracking.domain.exception.DomainException;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -11,12 +12,14 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Setter(AccessLevel.PRIVATE)
 @Getter
 public class Delivery {
 
+    @Id
     @EqualsAndHashCode.Include
     private UUID id;
 
@@ -35,9 +38,29 @@ public class Delivery {
 
     private Integer totalItems;
 
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "zipCode", column = @Column(name = "sender_zip_code")),
+            @AttributeOverride(name = "street", column = @Column(name = "sender_street")),
+            @AttributeOverride(name = "number", column = @Column(name = "sender_number")),
+            @AttributeOverride(name = "complement", column = @Column(name = "sender_complement")),
+            @AttributeOverride(name = "name", column = @Column(name = "sender_name")),
+            @AttributeOverride(name = "phone", column = @Column(name = "sender_phone"))
+    })
     private ContactPoint sender;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "zipCode", column = @Column(name = "recipient_zip_code")),
+            @AttributeOverride(name = "street", column = @Column(name = "recipient_street")),
+            @AttributeOverride(name = "number", column = @Column(name = "recipient_number")),
+            @AttributeOverride(name = "complement", column = @Column(name = "recipient_complement")),
+            @AttributeOverride(name = "name", column = @Column(name = "recipient_name")),
+            @AttributeOverride(name = "phone", column = @Column(name = "recipient_phone"))
+    })
     private ContactPoint recipient;
 
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "delivery")
     private List<Item> items = new ArrayList<>();
 
     public static Delivery draft(){
@@ -52,7 +75,7 @@ public class Delivery {
     }
 
     public UUID addItem(String name, int quantity){
-        Item item = Item.brandNew(name, quantity);
+        Item item = Item.brandNew(name, quantity,this);
         items.add(item);
         calculateTotalItems();
         return item.getId();
@@ -99,7 +122,7 @@ public class Delivery {
     }
 
     public void markAsDelivered() {
-        this.changeStatusTo(DeliveryStatus.DELIVERY);
+        this.changeStatusTo(DeliveryStatus.DELIVERED);
         this.setFulfilledAt(OffsetDateTime.now());
     }
 
